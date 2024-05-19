e7aca8d2dd6dbabbbb4f4112849cf379250aeef2
elena_yagodkina <engineerlena@mail.ru>
2024-05-19 14:38:56 +0300

    push_master

---
+++ src/test/java/ArrayList/arrayList/ArrayListApplicationTests.java
@@ -0,0 +1,13 @@
+package ArrayList.arrayList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArrayListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
